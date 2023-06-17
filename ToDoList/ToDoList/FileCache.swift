74da2f6dee08c39ed684f7d81973a68556ca8101
AzatSultan <136738095+AzatSultan@users.noreply.github.com>
2023-06-17 03:20:22 +0300

    newVersion

--- ToDoList/ToDoList/FileCache.swift
+++ ToDoList/ToDoList/FileCache.swift
@@ -1,54 +1,19 @@
 
 import Foundation
 
+protocol JsonFunc {
+    var dictToDo: [String: ToDoItem] { get }
+    func add(toDoItem: ToDoItem)
+    
+    func remove(id: String)
+}
+
 private enum FileFormat: String {
     case json = ".json"
     case csv = ".csv"
 }
 
-extension FileCache {
-    func loadFromJSON(name: String) {
-        if let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
-            let pathToFile = documentDirectory.appendingPathComponent(name + FileFormat.json.rawValue)
-            if let data = try? Data(contentsOf: pathToFile) {
-                if let jsonFile = try? JSONSerialization.jsonObject(with: data) as? [Any] {
-                    for jsonItem in jsonFile {
-                        if let parsedItem = ToDoItem.parse(json: jsonItem) {
-                            add(toDoItem: parsedItem)
-                        }
-                    }
-                } else {
-                    print("Не можем распарсить")
-                }
-            } else {
-                print("Файл поврежден")
-            }
-        } else {
-            print("Файл не найден")
-        }
-    }
-    
-    func saveToJSON(name: String) {
-        if let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
-            let pathToFile = documentDirectory.appendingPathComponent(name + FileFormat.json.rawValue)
-            let jsonItem = dictToDo.map {
-                $0.value.json
-            }
-            if let data = try? JSONSerialization.data(withJSONObject: jsonItem) {
-                guard ((try? data.write(to: pathToFile)) != nil) else {
-                    print("Ошибка при сохранении файла")
-                    return
-                }
-            } else {
-                print("Ошибка преобразования в json")
-            }
-        } else {
-            print("Директория не найдена")
-        }
-    }
-}
-
-class FileCache {
+class FileCache: JsonFunc {
     private(set) var dictToDo: [String: ToDoItem] = [:]
     
     func add(toDoItem: ToDoItem) {
@@ -56,5 +21,48 @@ class FileCache {
     }
     func remove(id: String) {
         dictToDo[id] = nil
+    }
+}
+
+extension FileCache {
+    func loadFromJSON(name: String) {
+        guard let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else {
+            print("Файл не найден")
+            return
+        }
+        let pathToFile = documentDirectory.appendingPathComponent(name + FileFormat.json.rawValue)
+        guard let data = try? Data(contentsOf: pathToFile) else {
+            print("Файл поврежден")
+            return
+        }
+        guard let jsonFile = try? JSONSerialization.jsonObject(with: data) as? [Any] else {
+            print("Не можем распарсить")
+            return
+        }
+
+        for jsonItem in jsonFile {
+            if let parsedItem = ToDoItem.parse(json: jsonItem) {
+                add(toDoItem: parsedItem)
+            }
+        }
+    }
+    
+    func saveToJSON(name: String) {
+        guard let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else {
+            print("Директория не найдена")
+            return
+        }
+        let pathToFile = documentDirectory.appendingPathComponent(name + FileFormat.json.rawValue)
+        let jsonItem = dictToDo.map {
+            $0.value.json
+        }
+        guard let data = try? JSONSerialization.data(withJSONObject: jsonItem) else {
+            print("Ошибка преобразования в json")
+            return
+        }
+        guard ((try? data.write(to: pathToFile)) != nil) else {
+            print("Ошибка при сохранении файла")
+            return
+        }
     }
 }
